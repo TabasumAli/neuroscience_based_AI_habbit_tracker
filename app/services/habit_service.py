88d686109976cf85da729370520481ea3da20005
Tabasum Ali <120305067+TabasumAli@users.noreply.github.com>
2025-08-24 03:19:06 +0500

    Update habit_service.py

--- app/services/habit_service.py
+++ app/services/habit_service.py
@@ -1,3 +1,19 @@
+# from app.database.models import Habit
+# from app.database.session import SessionLocal
+
+# class HabitService:
+#     def __init__(self):
+#         self.session = SessionLocal()
+
+#     def add_habit(self, habit_name):
+#         habit = Habit(habit_name=habit_name)
+#         self.session.add(habit)
+#         self.session.commit()
+#         return habit
+
+#     def get_habits(self):
+#         return self.session.query(Habit).all()
+
 from app.database.models import Habit
 from app.database.session import SessionLocal
 
@@ -5,11 +21,11 @@ class HabitService:
     def __init__(self):
         self.session = SessionLocal()
 
-    def add_habit(self, habit_name):
-        habit = Habit(habit_name=habit_name)
+    def add_habit(self, habit_name, user_id):
+        habit = Habit(habit_name=habit_name, user_id=user_id)
         self.session.add(habit)
         self.session.commit()
         return habit
 
-    def get_habits(self):
-        return self.session.query(Habit).all()
+    def get_habits(self, user_id):
+        return self.session.query(Habit).filter(Habit.user_id == user_id).all()
