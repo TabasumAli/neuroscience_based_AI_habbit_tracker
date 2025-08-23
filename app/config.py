7fb22198acbe265f0d268ddde66c32765f9ac774
Tabasum Ali <120305067+TabasumAli@users.noreply.github.com>
2025-08-24 03:53:31 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 # Load .env
 load_dotenv()
 
-DB_URL = "sqlite:////tmp/habit_tracker.db"
+DB_URL = "sqlite:////mount/src/neuroscience_based_ai_habbit_tracker/habit_tracker.db"
 AIMLAPI_KEY = os.getenv("AIMLAPI_KEY")
