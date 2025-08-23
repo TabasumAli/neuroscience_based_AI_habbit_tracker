e4943f1ac0957905b8bd4b19403e04ea288b4d8c
Tabasum Ali <120305067+TabasumAli@users.noreply.github.com>
2025-08-24 04:27:04 +0500

    Update session.py

--- app/database/session.py
+++ app/database/session.py
@@ -1,13 +1,7 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from app.config import DB_URL
-
-# engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
-# SessionLocal = sessionmaker(bind=engine)
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import DB_URL
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine)
+
